23b702f1a4499cf6ab90e42eeccc643f73e1cc16
Rafael Malanco <rafael.malanco@softtek.com>
2023-03-06 21:55:19 -0600

    Update

--- assets/js/puntoscambio.js
+++ assets/js/puntoscambio.js
@@ -1,277 +1,287 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var obj = {};
 
-    var obj ={};
+  $(".editar").click(function () {
+    var id = $(this).data("id");
+    var idLeng = $(this).data("idlenguaje");
+    var palabra = $(".nombrePal" + id).html();
+    var lenguaje = $(".nombreLeng" + id).html();
 
-    $('.editar').click(function(){
-        var id = $(this).data('id');
-        var idLeng = $(this).data('idlenguaje');
-        var palabra = $('.nombrePal'+id).html();
-        var lenguaje = $('.nombreLeng'+id).html();
-        
-        
-        $('.nombrePal'+id).html('<input name="editarNombrePal' + id + '" class="editarNombrepal'+ id +'" value = "' + palabra +  '" />');
-        //$('.nombreLeng'+id).html('<input name="editarNombreLeng' + id + '" class= "editarNombreLeng'+ id +'" value = "' + lenguaje + '"/>');
+    $(".nombrePal" + id).html(
+      '<input name="editarNombrePal' +
+        id +
+        '" class="editarNombrepal' +
+        id +
+        '" value = "' +
+        palabra +
+        '" />'
+    );
+    //$('.nombreLeng'+id).html('<input name="editarNombreLeng' + id + '" class= "editarNombreLeng'+ id +'" value = "' + lenguaje + '"/>');
 
-        $('.editar'+id).hide();
-        $('.eliminar'+id).hide();
+    $(".editar" + id).hide();
+    $(".eliminar" + id).hide();
 
-        $('.save'+id).show();
-        $('.cancelar'+id).show();
+    $(".save" + id).show();
+    $(".cancelar" + id).show();
 
-        $('.editar_acciones_'+id).hide();
-        $('.editar_acciones_cancelar'+id).removeAttr('style');
+    $(".editar_acciones_" + id).hide();
+    $(".editar_acciones_cancelar" + id).removeAttr("style");
 
-        obj.idLeng = idLeng;
-        obj.idtd = id;
-        obj.url = '../puntoscambio/crearSelect';
-        obj.data = {
-        };
+    obj.idLeng = idLeng;
+    obj.idtd = id;
+    obj.url = "../puntoscambio/crearSelect";
+    obj.data = {};
 
-        obj.type = 'POST';
-        obj.accion = 'crearSelect';
+    obj.type = "POST";
+    obj.accion = "crearSelect";
 
-        peticionAjax(obj);
-    });
+    peticionAjax(obj);
+  });
 
-    $('.cancelar').click(function(){
-        var id = $(this).data('id');
-        var nombre = $(this).data('nombre');
-        var lenguaje = $(this).data('lenguaje');
+  $(".cancelar").click(function () {
+    var id = $(this).data("id");
+    var nombre = $(this).data("nombre");
+    var lenguaje = $(this).data("lenguaje");
 
-        $('.nombrePal'+id).html(nombre);
-        $('.nombreLeng'+id).html(lenguaje);
+    $(".nombrePal" + id).html(nombre);
+    $(".nombreLeng" + id).html(lenguaje);
 
-        $('.editar'+id).show();
-        $('.eliminar'+id).show();
+    $(".editar" + id).show();
+    $(".eliminar" + id).show();
 
-        $('.save'+id).hide();
-        $('.cancelar'+id).hide();
+    $(".save" + id).hide();
+    $(".cancelar" + id).hide();
+  });
 
-    });
+  $(".save").click(function () {
+    var id = $(this).data("id");
+    var nombrePal = $(".editarNombrepal" + id).val();
+    var nombreLeng = $(".language" + id).val();
+    var lenguaje = $(".recuperarNombre" + nombreLeng).data("nombreleng");
 
-    $('.save').click(function(){
-        var id = $(this).data('id');
-        var nombrePal = $('.editarNombrepal' + id).val();
-        var nombreLeng = $('.language' + id).val();
-        var lenguaje = $('.recuperarNombre' +nombreLeng).data("nombreleng");
+    if (nombrePal == "") {
+      $(".mensaje_sistema").html("El campo palabra no puede estar vacio");
+      $(".mensaje").addClass("bg-warning");
+      $("#mensajeModal").modal("show");
+      exit();
+    }
+    if (nombreLeng == "") {
+      $(".mensaje_sistema").html("El campo lenguaje no puede estar vacio");
+      $(".mensaje").addClass("bg-warning");
+      $("#mensajeModal").modal("show");
+      exit();
+    }
 
-        if(nombrePal == ''){
-            $('.mensaje_sistema').html('El campo palabra no puede estar vacio');
-            $('.mensaje').addClass("bg-warning");
-            $('#mensajeModal').modal('show');
-            exit();
-        }
-        if(nombreLeng == ''){
-            $('.mensaje_sistema').html('El campo lenguaje no puede estar vacio');
-            $('.mensaje').addClass("bg-warning");
-            $('#mensajeModal').modal('show');
-            exit();
-        }
+    $(".nombrePal" + id).html(nombrePal);
+    $(".nombreLeng" + id).html(lenguaje);
+    $(".save" + id).hide();
+    $(".cancelar" + id).hide();
+    $(".editar" + id).show();
+    $(".eliminar" + id).show();
 
-        $('.nombrePal' + id).html(nombrePal);
-        $('.nombreLeng' + id).html(lenguaje);
-        $('.save' + id).hide();
-        $('.cancelar' + id).hide();
-        $('.editar' + id).show();
-        $('.eliminar' + id).show();
+    obj.url = "../puntoscambio/update";
+    obj.data = {
+      id: id,
+      nombre_pal: nombrePal,
+      nombre_leng: nombreLeng,
+    };
 
-        obj.url = '../puntoscambio/update';
-        obj.data = {
-            id: id,
-            nombre_pal: nombrePal,
-            nombre_leng: nombreLeng
-        };
+    obj.type = "POST";
+    obj.accion = "update";
 
-        obj.type = 'POST';
-        obj.accion = 'update';
+    peticionAjax(obj);
+  });
 
-        peticionAjax(obj);
+  $(".eliminar").click(function () {
+    var id = $(this).data("id");
+    $(".eliminarFila" + id).hide();
+    obj.url = "../puntoscambio/delete";
+    obj.data = {
+      id: id,
+    };
 
-    });
+    obj.type = "POST";
+    obj.accion = "delete";
 
-    $(".eliminar").click(function(){
-        var id = $(this).data('id');
-        $(".eliminarFila"+id).hide();
-        obj.url = '../puntoscambio/delete';
-        obj.data = {
-            id: id
-        };
+    peticionAjax(obj);
+  });
 
-        obj.type = 'POST';
-        obj.accion = 'delete';
+  $(".guardarPalabra").click(function () {
+    event.preventDefault();
+    var palabra = $("#palabra").val();
+    var datos = $("#formPalabra").serialize();
 
-        peticionAjax(obj);
-    });
+    if (palabra == "") {
+      $(".mensaje_sistema").html("El campo palabra no puede estar vacio");
+      $(".mensaje").addClass("bg-warning");
+      $("#mensajeModal").modal("show");
+      exit();
+    }
 
-    $(".guardarPalabra").click(function(){
-        event.preventDefault();
-        var palabra = $("#palabra").val();
-        var datos = $("#formPalabra").serialize();
-        
+    obj.url = "../puntoscambio/savePalabra";
+    obj.data = datos;
+    obj.type = "POST";
+    obj.accion = "savePalabra";
 
-        if(palabra == ''){
-            $('.mensaje_sistema').html('El campo palabra no puede estar vacio');
-            $('.mensaje').addClass("bg-warning");
-            $('#mensajeModal').modal('show');
-            exit();
-        }
+    $("#formPalabra")[0].reset();
 
-        obj.url = '../puntoscambio/savePalabra';
-        obj.data = datos;
-        obj.type = 'POST';
-        obj.accion = 'savePalabra';
+    peticionAjax(obj);
+  });
 
-        $('#formPalabra')[0].reset();
+  $(".guardarLenguaje").click(function () {
+    event.preventDefault();
+    var lenguaje = $("#lenguaje").val();
+    var lenguajeExt = $("#lenguajeExtension").val();
+    if (lenguaje == "") {
+      $(".mensaje_sistema").html("El campo lenguaje no puede estar vacio");
+      $(".mensaje").addClass("bg-warning");
+      $("#mensajeModal").modal("show");
+      exit();
+    }
+    if (lenguajeExt == "") {
+      $(".mensaje_sistema").html("El campo de extension no puede estar vacio");
+      $(".mensaje").addClass("bg-warning");
+      $("#mensajeModal").modal("show");
+      exit();
+    }
 
-        peticionAjax(obj);
+    obj.url = "../puntoscambio/saveLanguage";
+    obj.data = {
+      lenguaje: lenguaje,
+      lenguajeExtension: lenguajeExt,
+    };
+    obj.type = "POST";
+    obj.accion = "saveLanguage";
 
+    peticionAjax(obj);
+  });
 
-    });
+  $(".agregarNuevo").click(function () {
+    $(".crearModal").modal("show");
+  });
 
-    $(".guardarLenguaje").click(function(){
-        event.preventDefault();
-        var lenguaje = $("#lenguaje").val();
-        var lenguajeExt = $("#lenguajeExtension").val();
-        if(lenguaje == ''){
-            $('.mensaje_sistema').html('El campo lenguaje no puede estar vacio');
-            $('.mensaje').addClass("bg-warning");
-            $('#mensajeModal').modal('show');
-            exit();
-        }
-        if(lenguajeExt == ''){
-            $('.mensaje_sistema').html('El campo de extension no puede estar vacio');
-            $('.mensaje').addClass("bg-warning");
-            $('#mensajeModal').modal('show');
-            exit();
-        }
+  $(".cerrarModal").click(function () {
+    $(".crearModal").modal("hide");
+  });
 
-        obj.url = '../puntoscambio/saveLanguage';
-        obj.data = {
-            lenguaje: lenguaje,
-            lenguajeExtension: lenguajeExt
-        };
-        obj.type = 'POST';
-        obj.accion = 'saveLanguage';
+  $("#formzip").on("submit", function (e) {
+    e.preventDefault();
+    var f = $(this);
+    var formData = new FormData(document.getElementById("formzip"));
+    $.ajax({
+      url: "../puntoscambio/leerZip",
+      type: "post",
+      dataType: "json",
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+    }).done(function (res) {
+      var tabla = "";
+      tabla += '<table class="table" border="1">';
+      tabla += "<thead> <tr>";
+      tabla += "<th>Nombre del Archivo</th>";
+      //console.log(res);
+      //console.log(res.result[0].if);
+      $.each(res.thead, function (key, data) {
+        tabla += "<th>" + data + "</th>";
+      });
 
-        peticionAjax(obj);
-    });
+      tabla += "<th> Total </th>";
+      tabla += "</tr> </thead>";
+      tabla += "<tbody> ";
+      var result = res.result;
+      var contadorPC = 0;
 
-    $(".agregarNuevo").click(function(){
-        $(".crearModal").modal("show");
-    });
-
-    $('.cerrarModal').click(function(){
-        $('.crearModal').modal("hide");
-    });
-
-    $("#formzip").on("submit",function(e){
-        e.preventDefault();
-        var f = $(this);
-        var formData = new FormData(document.getElementById("formzip"));
-        $.ajax({
-            url: "../puntoscambio/leerZip",
-            type: "post",
-            dataType: 'json',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-        })
-        .done(function(res){
-            var tabla='';
-            tabla += '<table class="table" border="1">';
-            tabla += '<thead> <tr>';
-            tabla += '<th>Nombre del Archivo</th>';
-            //console.log(res);
-            //console.log(res.result[0].if);
-            $.each(res.thead, function(key,data){
-                tabla += "<th>" + data + "</th>";
-            });
-
-            tabla += "<th> Total </th>";
-            tabla += '</tr> </thead>';
-            tabla += '<tbody> ';
-            var result = res.result;
-            var contadorPC = 0;
-            
-            $.each(res.NombreArchivo, function(keyNombre,dataNombre){
-                tabla += '<tr>';
-                tabla += '<td>' + dataNombre + '</td>';
-                $.each(res.thead, function(key,data){
-                    tabla += '<td>' + result[keyNombre][data] + '</td>';
-                    contadorPC +=  result[keyNombre][data];
-                });
-                tabla += '<td>' + contadorPC + '</td>';
-                tabla += '</tr>';
-                contadorPC = 0
-            });
-            tabla += '</tbody>';
-
-            tabla += '</table>';
-            $('#creartabla').html(tabla);
-           
+      $.each(res.NombreArchivo, function (keyNombre, dataNombre) {
+        tabla += "<tr>";
+        tabla += "<td>" + dataNombre + "</td>";
+        $.each(res.thead, function (key, data) {
+          tabla += "<td>" + result[keyNombre][data] + "</td>";
+          contadorPC += result[keyNombre][data];
         });
+        tabla += "<td>" + contadorPC + "</td>";
+        tabla += "</tr>";
+        contadorPC = 0;
+      });
+      tabla += "</tbody>";
+
+      tabla += "</table>";
+      $("#creartabla").html(tabla);
     });
-    
+  });
 });
 
 function peticionAjax(datos) {
-    $.ajax({
-        url: datos.url,
-        data: datos.data,
-        cache: false,
-        type: datos.type,
-        contentType: false,
-        processData: false,      
-        dataType: 'json',
-        success: function(res) {
-            switch (datos.accion) {
-                case "savePalabra":
-                    $('.mensaje_sistema').html(res);
-                    $("#mensajeModal").modal("show");
-                    
-                    
-                                      
-                    break;    
+  $.ajax({
+    url: datos.url,
+    data: datos.data,
+    cache: false,
+    type: datos.type,
+    contentType: false,
+    processData: false,
+    dataType: "json",
+    success: function (res) {
+      switch (datos.accion) {
+        case "savePalabra":
+          $(".mensaje_sistema").html(res);
+          $("#mensajeModal").modal("show");
 
-                case "saveLanguage":
-                    $('.mensaje_sistema').html(res);
-                    $("#mensajeModal").modal("show");
-                    
-                break;
+          break;
 
-                case "update":
-                    $('.mensaje_sistema').html(res.res);
-                    $("#mensajeModal").modal("show");
+        case "saveLanguage":
+          $(".mensaje_sistema").html(res);
+          $("#mensajeModal").modal("show");
 
-                    break;
+          break;
 
-                case "delete":
-                    $('.mensaje_sistema').html(res);
-                    $("#mensajeModal").modal("show");
-                    break;
+        case "update":
+          $(".mensaje_sistema").html(res.res);
+          $("#mensajeModal").modal("show");
 
-                case "crearSelect":
-                    var crearSelect = '';
-                    crearSelect += '<select class="form-select language'+datos.idtd+'" aria-label="Default select example">';
-                    $.each(res.id, function(key,data){
-                        if(Number(data) == Number(datos.idLeng)){
-                            crearSelect += '<option value="'+data+'" class="recuperarNombre'+data+'" data-nombreleng="'+res.nombre[key]+'" selected>'+res.nombre[key]+'</option>'; 
-                        }else{
-                            crearSelect += '<option value="'+data+'" class="recuperarNombre'+data+'" data-nombreleng="'+res.nombre[key]+'" >'+res.nombre[key]+'</option>';
-                        }
-                    });
-                    crearSelect += '</select>';
-                    $('.nombreLeng'+datos.idtd).html(crearSelect);
-                    break;
+          break;
 
+        case "delete":
+          $(".mensaje_sistema").html(res);
+          $("#mensajeModal").modal("show");
+          break;
 
+        case "crearSelect":
+          var crearSelect = "";
+          crearSelect +=
+            '<select class="form-select language' +
+            datos.idtd +
+            '" aria-label="Default select example">';
+          $.each(res.id, function (key, data) {
+            if (Number(data) == Number(datos.idLeng)) {
+              crearSelect +=
+                '<option value="' +
+                data +
+                '" class="recuperarNombre' +
+                data +
+                '" data-nombreleng="' +
+                res.nombre[key] +
+                '" selected>' +
+                res.nombre[key] +
+                "</option>";
+            } else {
+              crearSelect +=
+                '<option value="' +
+                data +
+                '" class="recuperarNombre' +
+                data +
+                '" data-nombreleng="' +
+                res.nombre[key] +
+                '" >' +
+                res.nombre[key] +
+                "</option>";
             }
-        },
-        error: function(xhr, estatus) {
-
-        }
-    });
-
-
+          });
+          crearSelect += "</select>";
+          $(".nombreLeng" + datos.idtd).html(crearSelect);
+          break;
+      }
+    },
+    error: function (xhr, estatus) {},
+  });
 }
